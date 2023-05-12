ce17ae1e7113f51c8e7f3f0debbc3be9117e6fe6
roamingterra <danielballerini1@gmail.com>
2023-05-12 12:27:18 -0400

    Add state to skills components
    Skills can now be typed into the skills fields, and are added to state
    after clicking the Enter key. Once added, the fields become cleared of
    the text. Next, I need to make these skills appear as elements above the
    skills component.

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,11 +1,74 @@
 import React, { Component } from "react";
 
 class Skills extends Component {
-  // constructor{
-  //     super();
+  constructor() {
+    super();
 
-  //     this.state = {}
-  // }
+    this.state = {
+      programmingLanguages: [],
+      frameworksAndLibraries: [],
+      tools: [],
+      languagesSpoken: [],
+    };
+  }
+
+  addProgrammingLanguage = (event) => {
+    if (event.key === "Enter") {
+      this.setState(
+        (prevState) => ({
+          programmingLanguages: [
+            ...prevState.programmingLanguages,
+            event.target.value,
+          ],
+        }),
+        () => {
+          event.target.value = "";
+        }
+      );
+    }
+  };
+
+  addFramework = (event) => {
+    if (event.key === "Enter") {
+      this.setState(
+        (prevState) => ({
+          frameworksAndLibraries: [
+            ...prevState.frameworksAndLibraries,
+            event.target.value,
+          ],
+        }),
+        () => {
+          event.target.value = "";
+        }
+      );
+    }
+  };
+
+  addTool = (event) => {
+    if (event.key === "Enter") {
+      this.setState(
+        (prevState) => ({
+          tools: [...prevState.tools, event.target.value],
+        }),
+        () => {
+          event.target.value = "";
+        }
+      );
+    }
+  };
+
+  addLanguageSpoken = (event) => {
+    if (event.key === "Enter") {
+      this.setState(
+        (prevState) => ({
+          languagesSpoken: [...prevState.languagesSpoken, event.target.value],
+        }),
+        () => {
+          event.target.value = "";
+        }
+      );
+    }
+  };
 
   render() {
     return (
@@ -13,13 +76,13 @@ class Skills extends Component {
         <form>
           <div>
             <label>Programming Languages</label>
-            <input type="text"></input>
+            <input type="text" onKeyDown={this.addProgrammingLanguage}></input>
             <label>Frameworks and Libraries</label>
-            <input type="text"></input>
-            <label type="text">Tools</label>
-            <input type="text"></input>
-            <label type="text">Languages Spoken</label>
-            <input></input>
+            <input type="text" onKeyDown={this.addFramework}></input>
+            <label>Tools</label>
+            <input type="text" onKeyDown={this.addTool}></input>
+            <label>Languages Spoken</label>
+            <input type="text" onKeyDown={this.addLanguageSpoken}></input>
           </div>
         </form>
       </div>
